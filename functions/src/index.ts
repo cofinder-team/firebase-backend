7647c18986179658c1f106e71f4db783f3b35cf8
Seungjik Lee <lsjbh45@gmail.com>
2023-06-01 20:21:21 +0900

    feat: Initialize firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,23 @@
 import * as functions from 'firebase-functions';
 import express, { Request, Response } from 'express';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const store = admin.firestore();
 
 const app = express();
 
-app.get('/hello', (request: Request, response: Response) => {
-  response.send('Hello from Express on Firebase!');
+app.get('/', async (request: Request, response: Response) => {
+  const testRef = store.collection('test');
+  testRef.add({
+    createdAt: new Date(),
+  });
+  const data = await testRef
+    .get()
+    .then((snapshot) =>
+      snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })),
+    );
+  response.send(data);
 });
 
 export const api = functions.https.onRequest(app);
