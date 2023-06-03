01eb63ac4b84dafa4a6de565bb3e71bb92480c88
Seungjik Lee <lsjbh45@gmail.com>
2023-06-03 19:27:33 +0900

    feat: Add logger middleware for API request

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { errorHandler } from './lib/middleware/error.handler';
 import router from './controller';
+import { loggerMiddleware } from './lib/middleware/logger.middleware';
 
 const app = express();
+app.use(loggerMiddleware);
 app.use('/', router);
 app.use(errorHandler);
 
