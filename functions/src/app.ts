1a68252096bc70c84330e7bc27eb8eae4a4cc0d4
Seungjik Lee <lsjbh45@gmail.com>
2023-06-03 15:27:18 +0900

    fix: Resolve invalid sequence of error handler

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { errorHandler } from './lib/middleware/error.handler';
+import router from './controller';
 
 const app = express();
+app.use('/', router);
 app.use(errorHandler);
 
 export default app;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,5 @@
 import * as functions from 'firebase-functions';
 import app from './app';
-import router from './controller';
-
-app.use('/', router);
 
 const region = 'asia-northeast3';
 export const api = functions.region(region).https.onRequest(app);
