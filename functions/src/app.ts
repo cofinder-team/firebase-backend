fa0e9bc5f7aff1dc37ac7877d12d7899471f68c2
Seungjik Lee <lsjbh45@gmail.com>
2023-06-06 16:28:51 +0900

    feat: Enable cors for all origins

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import { errorHandler } from './lib/middleware/error.handler';
 import router from './controller';
 import { loggerMiddleware } from './lib/middleware/logger.middleware';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({ origin: true }));
 app.use(loggerMiddleware);
 app.use('/', router);
 app.use(errorHandler);
