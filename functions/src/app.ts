59bc03c2b534be6ded9497d79a1b447a9a904272
Seungjik Lee <lsjbh45@gmail.com>
2023-06-10 16:54:25 +0900

    feat: Add cors origin checking

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import { errorHandler } from './lib/middleware/error.handler';
 import router from './controller';
 import { loggerMiddleware } from './lib/middleware/logger.middleware';
+import * as functions from 'firebase-functions';
 import cors from 'cors';
 
+const config = functions.config();
+const origin = config.cors.origin;
+
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors({ methods: ['GET', 'POST'], origin }));
 app.use(loggerMiddleware);
 app.use('/', router);
 app.use(errorHandler);
