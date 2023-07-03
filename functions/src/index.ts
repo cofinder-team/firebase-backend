080484b5b725ea681710e171ddf4142b78927545
Seungjik Lee <lsjbh45@gmail.com>
2023-07-03 15:34:44 +0900

    fix: Maximize timeout second of firebase function processing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,4 +2,7 @@ import * as functions from 'firebase-functions';
 import app from './app';
 
 const region = 'asia-northeast3';
-export const api = functions.region(region).https.onRequest(app);
+export const api = functions
+  .region(region)
+  .runWith({ timeoutSeconds: 540 })
+  .https.onRequest(app);
