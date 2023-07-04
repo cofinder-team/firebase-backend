b68d72dfb3efec1be5a3947d6c24623211ff5992
Seungjik Lee <lsjbh45@gmail.com>
2023-07-04 10:27:40 +0900

    feat: Add ipad as collection target

--- functions/src/lib/coupang/targets.ts
+++ functions/src/lib/coupang/targets.ts
@@ -397,6 +397,210 @@ const targets = (): CoupangTarget[] => [
     productId: 7122228405,
     vendorIds: [84999598454, 84999598409],
   },
+  {
+    itemId: 6,
+    optionId: 1,
+    productId: 6091182064,
+    vendorIds: [78633105451, 78633105441, 78633105462, 78633105475],
+  },
+  {
+    itemId: 6,
+    optionId: 2,
+    productId: 6091182064,
+    vendorIds: [78633105724, 78633105715, 78633105735, 78633105747],
+  },
+  {
+    itemId: 6,
+    optionId: 3,
+    productId: 6091182064,
+    vendorIds: [78633105447, 78633105434, 78633105458, 78633105476],
+  },
+  {
+    itemId: 6,
+    optionId: 4,
+    productId: 6091182064,
+    vendorIds: [78633105564, 78633105554, 78633105572, 78633105580],
+  },
+
+  {
+    itemId: 7,
+    optionId: 5,
+    productId: 6396408893,
+    vendorIds: [
+      80912364522, 80912364543, 80912364508, 80912364495, 80912364532,
+    ],
+  },
+  {
+    itemId: 7,
+    optionId: 6,
+    productId: 6396408893,
+    vendorIds: [
+      80912364505, 80912364549, 80912364560, 80912364494, 80912364540,
+    ],
+  },
+  {
+    itemId: 7,
+    optionId: 7,
+    productId: 6396408893,
+    vendorIds: [
+      80912364612, 80912364604, 80912364595, 80912364575, 80912364583,
+    ],
+  },
+  {
+    itemId: 7,
+    optionId: 8,
+    productId: 6396408893,
+    vendorIds: [
+      80912364612, 80912364545, 80912364518, 80912364562, 80912364506,
+    ],
+  },
+
+  {
+    itemId: 8,
+    optionId: 5,
+    productId: 6091182002,
+    vendorIds: [78633105037, 78633105049],
+  },
+  {
+    itemId: 8,
+    optionId: 6,
+    productId: 6091182002,
+    vendorIds: [78633105198, 78633105186],
+  },
+  {
+    itemId: 8,
+    optionId: 7,
+    productId: 6091182002,
+    vendorIds: [78633105205, 78633105217],
+  },
+  {
+    itemId: 8,
+    optionId: 8,
+    productId: 6091182002,
+    vendorIds: [78633105534, 78633105544],
+  },
+  {
+    itemId: 8,
+    optionId: 9,
+    productId: 6912124565,
+    vendorIds: [83845338385, 83845338403, 83845338441, 83845338426],
+  },
+  {
+    itemId: 8,
+    optionId: 10,
+    productId: 6912124565,
+    vendorIds: [83845338294, 83845338320, 83845338365, 83845338341],
+  },
+  {
+    itemId: 8,
+    optionId: 11,
+    productId: 6912124565,
+    vendorIds: [83845338532, 83845338267, 83845338515, 83845338561],
+  },
+  {
+    itemId: 8,
+    optionId: 12,
+    productId: 6912124565,
+    vendorIds: [83845338475, 83845338547, 83845338460, 83845338498],
+  },
+
+  {
+    itemId: 9,
+    optionId: 17,
+    productId: 6912124802,
+    vendorIds: [83845340057, 83845340044],
+  },
+  {
+    itemId: 9,
+    optionId: 18,
+    productId: 6912124802,
+    vendorIds: [83845340031, 83845340017],
+  },
+  {
+    itemId: 9,
+    optionId: 19,
+    productId: 6912124802,
+    vendorIds: [83845339975, 83845339962],
+  },
+  {
+    itemId: 9,
+    optionId: 20,
+    productId: 6912124802,
+    vendorIds: [83845340001, 83845339988],
+  },
+  {
+    itemId: 9,
+    optionId: 21,
+    productId: 6912124802,
+    vendorIds: [83845339889, 83845339919],
+  },
+  {
+    itemId: 9,
+    optionId: 22,
+    productId: 6912124802,
+    vendorIds: [83845339904, 83845339845],
+  },
+  {
+    itemId: 9,
+    optionId: 23,
+    productId: 6912124802,
+    vendorIds: [83845339877, 83845339861],
+  },
+  {
+    itemId: 9,
+    optionId: 24,
+    productId: 6912124802,
+    vendorIds: [83845339784, 83845339771],
+  },
+
+  {
+    itemId: 10,
+    optionId: 17,
+    productId: 6912124813,
+    vendorIds: [83845339992, 83845339981],
+  },
+  {
+    itemId: 10,
+    optionId: 18,
+    productId: 6912124813,
+    vendorIds: [83845339958, 83845339939],
+  },
+  {
+    itemId: 10,
+    optionId: 19,
+    productId: 6912124813,
+    vendorIds: [83845339925, 83845339874],
+  },
+  {
+    itemId: 10,
+    optionId: 20,
+    productId: 6912124813,
+    vendorIds: [83845339860, 83845339912],
+  },
+  {
+    itemId: 10,
+    optionId: 21,
+    productId: 6912124813,
+    vendorIds: [83845339806, 83845339787],
+  },
+  {
+    itemId: 10,
+    optionId: 22,
+    productId: 6912124813,
+    vendorIds: [83845339826, 83845340060],
+  },
+  {
+    itemId: 10,
+    optionId: 23,
+    productId: 6912124813,
+    vendorIds: [83845340045, 83845340088],
+  },
+  {
+    itemId: 10,
+    optionId: 24,
+    productId: 6912124813,
+    vendorIds: [83845340073, 8384534001],
+  },
 ];
 
 export default targets;
