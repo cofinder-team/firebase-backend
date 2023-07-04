3827c9d0edfcb7d6897656c0a7def003703fa6ea
Seungjik Lee <lsjbh45@gmail.com>
2023-07-04 09:39:55 +0900

    refactor: Disunite price update function from interface controller

--- functions/src/controller/controller.ts
+++ functions/src/controller/controller.ts
@@ -2,10 +2,7 @@ import { Request, Response, RequestHandler, NextFunction } from 'express';
 import store from '../lib/database/store';
 import { NotFoundException } from '../lib/exception/http.exception';
 import { sendEmailCollect } from '../lib/slack/slack';
-import targets from '../lib/coupang/targets';
-import collect from '../lib/coupang/collect';
-import { Coupang, CoupangTarget } from '../entity/coupang.entity';
-import delay from '../lib/util/delay';
+import update from '../lib/coupang/update';
 
 export const getItem: RequestHandler = async (
   req: Request,
@@ -133,41 +130,7 @@ export const updateCoupang: RequestHandler = async (
   next: NextFunction,
 ) => {
   try {
-    const collectEach = async (
-      promise: Promise<Coupang[]>,
-      target: CoupangTarget,
-    ): Promise<Coupang[]> => {
-      const collection = await promise;
-      await delay(Math.random() * 1000);
-
-      const result = await collect(target);
-      console.log(result);
-      return [...collection, result];
-    };
-
-    const updateEach = async (object: Coupang): Promise<void> => {
-      const { itemId, optionId, price, time } = object;
-      const date = new Date(time).toISOString().split('T')[0];
-
-      const items = await store
-        .collection('item')
-        .where('itemId', '==', itemId)
-        .where('optionId', '==', optionId)
-        .get();
-
-      await Promise.all(
-        items.docs.map(async (doc) => {
-          await doc.ref.collection('coupang').doc(date).set({
-            date,
-            price,
-          });
-        }),
-      );
-    };
-
-    const collection = await targets().reduce(collectEach, Promise.resolve([]));
-    const result = await Promise.all(collection.map(updateEach));
-
+    const result = await update();
     res.status(200).json(result);
   } catch (error) {
     next(error);
